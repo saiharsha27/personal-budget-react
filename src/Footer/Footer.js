8abebe740cce9f3b24edb83bb2b10c06fdaebe63
saiharsha paladugu <saiharshapaladugu@saiharshas-MacBook-Pro.local>
2024-09-29 22:36:05 -0400

    Brought PBudget code in

---
+++ src/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div className="bottom">
+    <div className="center">
+        <p role="contentinfo">All rights reserved &copy; Sai Harsha Paladugu</p>
+    </div>
+</div>
+  );
+}
+
+export default Footer;
